a20b4f83a982bbed155711453fb983d268604803
torwek <dariusz.kosmider@onet.pl>
2022-03-04 21:04:51 +0100

    Responsywa

--- dbconfig.js
+++ dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
     host: 'sql11.freemysqlhosting.net',
-    database: 'sql11473831',
-    user: 'sql11473831',
-    password: 'b38VyGAJu9'
+    database: 'sql11476894',
+    user: 'sql11476894',
+    password: '7cRy9WgpGP'
 }
